b3e5227450d9a95b54433c3017f4eadff198a63a
Gs-G2 <gsouzagomes616@gmail.com>
2024-08-22 10:38:53 -0300

    a

--- Aula3/src/br/com/controle/Compra.java
+++ Aula3/src/br/com/controle/Compra.java
@@ -10,6 +10,8 @@ package br.com.controle;
  */
 public class Compra {
     private double valor;
+    private double soma;
+    private double maior;
 
     public double getValor() {
         return valor;
@@ -17,5 +19,18 @@ public class Compra {
 
     public void setValor(double valor) {
         this.valor = valor;
+        if (this.valor > this. maior) {
+            this.maior = this.valor;
+        }
+        this.soma += this.valor;
     }
+
+    public double getSoma() {
+        return soma;
+    }
+
+    public double getMaior() {
+        return maior;
+    }
+    
 }
--- Aula3/src/br/com/visao/Exercicio.java
+++ Aula3/src/br/com/visao/Exercicio.java
@@ -14,20 +14,12 @@ import javax.swing.JOptionPane;
 public class Exercicio {
     public static void main(String[] args) {
         Compra c = new Compra();
-        double max = 0;
-        double soma = 0;
         
-    
         for (int i = 0; i < 5; i++) {
             c.setValor(Double.valueOf(JOptionPane.showInputDialog("Insira o valor da compra")));
-            
-            soma += c.getValor();
-            if (max < c.getValor()) {
-                max = c.getValor();
-            }
         }
-        JOptionPane.showMessageDialog(null, "Valor das compras "+soma);
-        JOptionPane.showMessageDialog(null, "Valor da maior compra "+max);
+        JOptionPane.showMessageDialog(null, "Valor das compras "+c.getSoma());
+        JOptionPane.showMessageDialog(null, "Valor da maior compra "+c.getMaior());
     
     }
 }
